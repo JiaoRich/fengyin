eae3907b7d60d1b613b57cddbd262a229176b7e3
JiaoRich <2383358050@qq.com>
2026-09-11 17:15:28 +0800

    Upload tests/test_swam_classifier.cpp

---
+++ tests/test_swam_classifier.cpp
@@ -0,0 +1,14 @@
+#include "../native/Source/SwamPluginClassifier.h"
+#include <cassert>
+
+int main()
+{
+    using fengyin::SwamFamily;
+    using fengyin::SwamPluginClassifier;
+    assert(SwamPluginClassifier::classify("SWAM Alto Sax", "Audio Modeling") == SwamFamily::saxophone);
+    assert(SwamPluginClassifier::classify("SWAM Trumpet", "Audio Modeling") == SwamFamily::brass);
+    assert(SwamPluginClassifier::classify("SWAM Flute", "Audio Modeling") == SwamFamily::woodwind);
+    assert(SwamPluginClassifier::classify("SWAM Cello", "Audio Modeling") == SwamFamily::strings);
+    assert(SwamPluginClassifier::classify("Kontakt", "Native Instruments") == SwamFamily::notSwam);
+}
+
