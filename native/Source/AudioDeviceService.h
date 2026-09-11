46348c4572165d022294e9792ffa0e9ad8838fda
JiaoRich <2383358050@qq.com>
2026-09-11 17:14:06 +0800

    Upload native/Source/AudioDeviceService.h

---
+++ native/Source/AudioDeviceService.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <juce_audio_devices/juce_audio_devices.h>
+#include <juce_data_structures/juce_data_structures.h>
+
+namespace fengyin
+{
+struct AudioDeviceStatus
+{
+    bool ready = false;
+    juce::String deviceType;
+    juce::String deviceName;
+    double sampleRate = 0.0;
+    int bufferSize = 0;
+    double estimatedBufferLatencyMs = 0.0;
+    juce::String error;
+};
+
+class AudioDeviceService final
+{
+public:
+    AudioDeviceService();
+    ~AudioDeviceService() = default;
+
+    juce::String initialise();
+    [[nodiscard]] AudioDeviceStatus getStatus();
+    [[nodiscard]] juce::StringArray getAvailableDeviceTypes();
+    [[nodiscard]] juce::StringArray getAvailableOutputDevices(const juce::String& typeName);
+    juce::String selectDeviceType(const juce::String& typeName);
+    juce::String applyOutputSetup(const juce::String& outputName, double sampleRate, int bufferSize);
+    [[nodiscard]] juce::AudioDeviceManager& getDeviceManager() noexcept { return manager; }
+
+private:
+    juce::AudioIODeviceType* findType(const juce::String& typeName);
+    void saveSettings();
+
+    juce::AudioDeviceManager manager;
+    juce::ApplicationProperties properties;
+    juce::String lastError;
+};
+}
