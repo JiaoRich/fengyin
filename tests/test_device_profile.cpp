484d657ae08790d00311a0ce26876eb114c3e3f9
JiaoRich <2383358050@qq.com>
2026-09-11 17:15:19 +0800

    Upload tests/test_device_profile.cpp

---
+++ tests/test_device_profile.cpp
@@ -0,0 +1,15 @@
+#include "../native/Source/DeviceProfile.h"
+#include <cassert>
+
+int main()
+{
+    using fengyin::DeviceProfileMatcher;
+    assert(DeviceProfileMatcher::match("Roland Aerophone AE-30").id == "roland-aerophone");
+    assert(DeviceProfileMatcher::match("YAMAHA YDS-150").id == "yamaha-yds");
+    assert(DeviceProfileMatcher::match("Akai EWI USB").id == "akai-ewi");
+    assert(DeviceProfileMatcher::match("Aodyo Sylphyo Link").id == "aodyo-sylphyo");
+    const auto generic = DeviceProfileMatcher::match("USB MIDI Device");
+    assert(generic.id == "generic-wind-controller");
+    assert(generic.breathController == 2);
+}
+
